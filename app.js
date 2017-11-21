3c7c9c1af6b8586b8250dd61dcd0169bf39e6b4e
Brian Zelip <bzelip@gmail.com>
2017-11-21 07:36:34 -0500

    add npm start script

--- app.js
+++ app.js
@@ -111,7 +111,9 @@ app.get('/logout', function(req, res) {
   res.redirect('/');
 });
 
-app.listen(3000);
+app.listen(3000, () => {
+  console.log(`Express running → PORT 3000`);
+});
 
 // Simple route middleware to ensure user is authenticated.
 //   Use this route middleware on any resource that needs to be protected.  If
